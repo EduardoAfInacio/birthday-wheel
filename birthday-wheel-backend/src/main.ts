73ff88964314635f035c3e32d21ca1f1f23d2156
EduardoAfInacio <eduafinacio@gmail.com>
2026-01-09 18:02:10 -0300

    Enabling CORS.

--- birthday-wheel-backend/src/main.ts
+++ birthday-wheel-backend/src/main.ts
@@ -8,6 +8,10 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
